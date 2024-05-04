dd1bf3ecb927cab9274631c669d3e2441d251fa0
John M. <john.medlock.dev@gmail.com>
2024-05-03 21:22:42 -0500

    Final cleanup and updated packages

--- LightFeather.CodeChallenge.Infrastructure/Migrations/20240504011214_Initial.cs
+++ LightFeather.CodeChallenge.Infrastructure/Migrations/20240504011214_Initial.cs
@@ -1,41 +1,39 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace LightFeather.CodeChallenge.Infrastructure.Migrations
-{
-    /// <inheritdoc />
-    public partial class Initial : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "SubmitSupervisorEntities",
-                columns: table => new
-                {
-                    Id = table.Column<long>(type: "bigint", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
-                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
-                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
-                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
-                    PhoneNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
-                    Supervisor = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
-                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
-                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_SubmitSupervisorEntities", x => x.Id);
-                });
-        }
+namespace LightFeather.CodeChallenge.Infrastructure.Migrations;
 
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "SubmitSupervisorEntities");
-        }
+/// <inheritdoc/>
+public partial class Initial : Migration
+{
+    /// <inheritdoc/>
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.CreateTable(
+            name: "SubmitSupervisorEntities",
+            columns: table => new
+            {
+                Id = table.Column<long>(type: "bigint", nullable: false)
+                    .Annotation("SqlServer:Identity", "1, 1"),
+                FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                PhoneNumber = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
+                Supervisor = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()")
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_SubmitSupervisorEntities", x => x.Id);
+            });
+    }
+
+    /// <inheritdoc/>
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "SubmitSupervisorEntities");
     }
 }
